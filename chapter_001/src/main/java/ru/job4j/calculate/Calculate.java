9968eadbf805935ddf8628aa53429198c346b82e
Ilya <iliabezh@gmail.com>
2018-12-03 17:48:50 +0300

    1.3. JavaDoc. Code convention[#93320]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate task class
+ *@author Ilya B 
+ *@version $id$
+ *@since 0.1
+ */
 public class Calculate{
+	/**
+	 * SOUT text 
+	 *@param args nothing now
+	 */
 	public static void main (String[] args){
-		System.out.print("Hello world.");
+		System.out.println("Hello world.");
 	}
 }
