d4efc871f0322cd35412651923023adbba0a85d7
Ilya <iliabezh@gmail.com>
2018-12-09 21:24:17 +0300

    4.1. Глупый бот.[#93282]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+/**
+ * @author Beshmelnitsyn Ilia (parsentev@yandex.ru)
+ * @version 1
+ * @since 09.12.2018
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
