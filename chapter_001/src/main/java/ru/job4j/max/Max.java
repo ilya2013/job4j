dff89613de09a5e8faabe5772208735a248c2c61
Ilya <iliabezh@gmail.com>
2018-12-11 15:22:01 +0300

    4.2. Максимум из двух чисел[#93284]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.max;
+/*
+ *Вычисление максимального числа.
+ * @author Ilya B
+ * @version 1
+ * @since 11.12.18
+ */
+
+public class Max {
+    public int max(int first, int second){
+        return (first>second) ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+}
