a7594680812ed26215bcb6cf16129b59ecee0a96
Ilya <iliabezh@gmail.com>
2018-12-20 15:25:06 +0300

    2. Чтение кода и базовые ошибки.[#93369] Добавление модуля chapter_002

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 public class ArrayDuplicate {
     public String[] remove(String[] array) {
-        int doubleCount = 0;
+        int doubles = 0;
         String tmp;
         for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length - doubleCount;) {
+            for (int j = i + 1; j < array.length - doubles;) {
                 if (array[i].equals(array[j])) {
                     tmp = array[j];
-                    array[j] = array[array.length - doubleCount - 1];
-                    array[array.length - doubleCount - 1] = tmp;
-                    doubleCount++;
+                    array[j] = array[array.length - doubles - 1];
+                    array[array.length - doubles - 1] = tmp;
+                    doubles++;
                 } else {
                     j++;
                 }
             }
         }
-        return Arrays.copyOf(array, array.length - doubleCount);
+        return Arrays.copyOf(array, array.length - doubles);
     }
 }
