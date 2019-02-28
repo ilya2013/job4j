a0db379734c18e45b92657f91e453e50e138e777
Ilya <iliabezh@gmail.com>
2019-02-28 22:05:26 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- chapter_004/src/main/java/ru/job4j/stream/Profile.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profile.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,8 +12,16 @@ public class Profile {
     }
 
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream().map(profile -> profile.getAddress()).distinct().sorted(new Comparator<Address>() {
+
+            @Override
+            public int compare(Address o1, Address o2) {
+                return o1.getCity().compareTo(o2.getCity());
+            }
+        }).collect(Collectors.toList());
     }
+
+
 
     public Address getAddress() {
         return address;
--- chapter_004/src/test/java/ru/job4j/stream/ProfileTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfileTest.java
@@ -19,8 +19,24 @@ public class ProfileTest {
         Address address3 = new Address("Тула");
         List<Profile> profiles = Arrays.asList(new Profile(address1),
                 new Profile(address2),
-                 new Profile(address3));
+                new Profile(address3));
+        List<Address> result = new Profile(address1).collect(profiles);
+        List<Address> expected = Arrays.asList(address1,
+                address2,
+                address3);
+        assertThat(result, is(expected));
+    }
 
+    @Test
+    public void collectDistinctAddresses() {
+        Address address1 = new Address("Москва");
+        Address address2 = new Address("СПб");
+        Address address3 = new Address("Тула");
+        Address address4 = new Address("Тула");
+        List<Profile> profiles = Arrays.asList(new Profile(address1),
+                new Profile(address2),
+                new Profile(address3),
+                new Profile(address4));
         List<Address> result = new Profile(address1).collect(profiles);
         List<Address> expected = Arrays.asList(address1,
                 address2,
