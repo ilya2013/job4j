c36be765706adcbbd0e55cb1d7d94c0958e11f9a
Ilya <iliabezh@gmail.com>
2019-02-28 19:06:28 +0300

    Фильтрация учеников. [#110058]

---
+++ chapter_004/src/main/java/ru/job4j/stream/Student.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+
+public class Student {
+    private int score = 0;
+
+    public int getScore() {
+        return score;
+    }
+
+    public Student(int score) {
+        this.score = score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SchoolTest {
+    private List<Student> students = Arrays.asList(new Student(10)
+            , new Student(70)
+            , new Student(40)
+            , new Student(60)
+            , new Student(100)
+            , new Student(55)
+    );
+
+    @Test
+    public void collectAClass() {
+        List<Student> result;
+        Predicate<Student> isA = (student -> student.getScore() >= 70 && student.getScore() <= 100);
+        School school = new School();
+        result = school.collect(students, isA);
+        List expected = Arrays.asList(students.get(1), students.get(4));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void collectBClass() {
+        List<Student> result;
+        Predicate<Student> isB = (student -> student.getScore() >= 50 && student.getScore() < 70);
+        School school = new School();
+        result = school.collect(students, isB);
+        List expected = Arrays.asList(students.get(3), students.get(5));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void collectCClass() {
+        List<Student> result;
+        Predicate<Student> isC = (student -> student.getScore() < 50);
+        School school = new School();
+        result = school.collect(students, isC);
+        List expected = Arrays.asList(students.get(0), students.get(2));
+        assertThat(result, is(expected));
+    }
+}
