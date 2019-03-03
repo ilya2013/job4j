f211dc31982dc976d00ab0145015b1c98c4c7424
Ilya <iliabezh@gmail.com>
2019-03-03 13:52:17 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228][#117528]

---
+++ chapter_004/src/test/java/ru/job4j/stream/flatMapTaskTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
+import static org.junit.Assert.*;
+
+public class flatMapTaskTest {
+
+    @Test
+    public void convert2x2() {
+        Integer[][] matrix = {{2,7},{3,6}};
+        List<Integer> expected = new ArrayList<>();
+        expected.add(2);
+        expected.add(7);
+        expected.add(3);
+        expected.add(6);
+        List<Integer> result =new FlatMapTask().convert(matrix);
+        assertThat(result, is(expected));
+    }
+}
