83356531e8ade8968af216ff761ac333db24222f
Ilya <iliabezh@gmail.com>
2019-03-25 16:22:40 +0300

    5.1.4. Создать convert(Iterator<Iterator>)[#115738]

--- chapter_005/src/main/java/ru/job4j/iterator/IteratorOfIterators.java
+++ chapter_005/src/main/java/ru/job4j/iterator/IteratorOfIterators.java
@@ -1,20 +1,25 @@
 package ru.job4j.iterator;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 
 public class IteratorOfIterators {
-    public class Converter {
-        Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
-            return new Iterator<Integer>() {
-                @Override
-                public boolean hasNext() {
-                    return false;
+    private Iterator<Integer> innerIterator;
+    protected Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                innerIterator = innerIterator == null || (!innerIterator.hasNext() && it.hasNext()) ? it.next() : innerIterator;
+                return innerIterator != null && innerIterator.hasNext();
+            }
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
                 }
-                @Override
-                public Integer next() {
-                    return null;
-                }
-            };
-        }
+                return innerIterator.next();
+            }
+        };
     }
 }
--- chapter_005/src/test/java/ru/job4j/iterator/IteratorOfIteratorsTest.java
+++ chapter_005/src/test/java/ru/job4j/iterator/IteratorOfIteratorsTest.java
@@ -1,5 +1,4 @@
 package ru.job4j.iterator;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -7,25 +6,26 @@ import java.util.NoSuchElementException;
 
 import org.junit.Before;
 import org.junit.Test;
+import ru.job4j.iterator.IteratorOfIterators;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class ConverterTest {
+public class IteratorOfIteratorsTest {
     Iterator<Integer> it;
 
     @Before
-    public void setUp () {
+    public void setUp() {
         Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
         Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
         Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-        Converter IteratorOfIterators = new Converter();
-        it = IteratorOfIterators.convert(its);
+        IteratorOfIterators iteratorOfIterators = new IteratorOfIterators();
+        it = iteratorOfIterators.convert(its);
     }
 
     @Test
-    public void hasNextNextSequentialInvocation () {
+    public void hasNextNextSequentialInvocation() {
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
         assertThat(it.hasNext(), is(true));
@@ -48,7 +48,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation () {
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(2));
         assertThat(it.next(), is(3));
@@ -61,7 +61,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder () {
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
         assertThat(it.hasNext(), is(true));
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
@@ -76,26 +76,26 @@ public class ConverterTest {
     }
 
     @Test
-    public void hasNextShouldReturnFalseInCaseOfEmptyIterators(){
+    public void hasNextShouldReturnFalseInCaseOfEmptyIterators() {
         Iterator<Integer> it1 = (new ArrayList<Integer>()).iterator();
         Iterator<Integer> it2 = (new ArrayList<Integer>()).iterator();
         Iterator<Integer> it3 = (new ArrayList<Integer>()).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-        Converter IteratorOfIterators = new Converter();
-        it = IteratorOfIterators.convert(its);
+        IteratorOfIterators iteratorOfIterators = new IteratorOfIterators();
+        it = iteratorOfIterators.convert(its);
         assertThat(it.hasNext(), is(false));
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void invocationOfNextMethodShouldThrowNoSuchElementException(){
-        Iterator<Integer> it1 = Arrays.asList(1,2,3).iterator();
+    public void invocationOfNextMethodShouldThrowNoSuchElementException() {
+        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1).iterator();
-        Converter IteratorOfIterators = new Converter();
-        it = IteratorOfIterators.convert(its);
+        IteratorOfIterators iteratorOfIterators = new IteratorOfIterators();
+        it = iteratorOfIterators.convert(its);
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(2));
         assertThat(it.next(), is(3));
         it.next();
     }
-
 }
+
