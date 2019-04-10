e524652a38a97aea4fc0c18f2fe68532da1ae317
Ilya <iliabezh@gmail.com>
2019-04-10 13:28:18 +0300

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#115730]

--- chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
@@ -1,38 +1,13 @@
 package ru.job4j.list;
 
 public class SimpleStack<T>  {
-    private int size;
-    private SimpleStack.Node<T> first;
+    private final SimpleArrayList<T> stack = new SimpleArrayList<>();
 
     public T poll() {
-        T result = first != null ? first.date : null;
-            this.first = this.first != null && this.first.next != null ? this.first.next : null;
-            this.size--;
-        return result;
+        return stack.delete();
     }
 
     public void push(T value) {
-        Node<T> newLink = new Node<>(value);
-        newLink.next = this.first;
-        this.first = newLink;
-        this.size++;
+        stack.add(value);
     };
-
-    /**
-     * Класс предназначен для хранения данных.
-     */
-    private static class Node<E> {
-
-        E date;
-        SimpleStack.Node<E> next;
-
-        Node(E date) {
-            this.date = date;
-        }
-
-        @Override
-        public String toString() {
-            return date.toString();
-        }
-    }
 }
