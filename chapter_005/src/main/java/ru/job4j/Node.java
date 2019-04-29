89ce56aaa6405562f10155626c1dedf3c5cf72a3
Ilya <iliabezh@gmail.com>
2019-04-29 18:24:35 +0300

    5.3.4. Задан связанный список. Определить цикличность.

--- chapter_005/src/main/java/ru/job4j/Node.java
+++ chapter_005/src/main/java/ru/job4j/Node.java
@@ -7,13 +7,10 @@ class Node<T> {
     public Node(T value) {
         this.value = value;
     }
-
     public static boolean hasCycle(Node first) {
         Node turtle = first;
         Node hare = first;
-
         while (hare != null && hare.next != null) {
-            //System.out.printf("turtle: %s, hare: %s%s", turtle.value, hare.value, System.lineSeparator());
             turtle = turtle.next;
             hare = hare.next.next;
             if (turtle == hare) {
